1abd800cf5c1718a3890e76c6796e45f50719aba
AlishaKausheen <alishakausheen@gmail.com>
2024-04-12 04:38:49 -0700

    mongoose work

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+require('dotenv').config();
+const PORT = process.env.PORT; 
+
 
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Task Manager API is working'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
