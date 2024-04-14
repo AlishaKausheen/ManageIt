14634d6ae383293d3b8827233c4979b78cb98e3d
AlishaKausheen <alishakausheen@gmail.com>
2024-04-14 04:11:00 -0700

    task.js managing schema

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 
 const router = express.Router();
+const auth = require('../middlewares/auth');
 
-router.get('/', (req, res) => {
-    res.send('Task routes are working');
+router.get('/test',auth, (req, res) => {
+    res.json({
+        message: 'Task routes are working',
+        user: req.user
+    })
 })
 
 //CRUD Operation for authenticated users
 
+//create a task
+router.post('/createtask', auth, async (req, res) => {
+    
+})
 
 module.exports = router;
