4fe770a2b53f8b6b04bb2f18b84203e65b4f9e21
AlishaKausheen <alishakausheen@gmail.com>
2024-04-14 04:26:06 -0700

    task creation

--- models/Task.js
+++ models/Task.js
@@ -5,7 +5,9 @@ const taskSchema = new mongoose.Schema({
     description: { type: String, required: true },
     completed: { type: Boolean, default: false},
     owner: {
-        
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 })
 
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const auth = require('../middlewares/auth');
+const Task = require('../models/Task');
 
 router.get('/test',auth, (req, res) => {
     res.json({
@@ -14,7 +15,19 @@ router.get('/test',auth, (req, res) => {
 
 //create a task
 router.post('/createtask', auth, async (req, res) => {
-    
+    try {
+        //description , completed frm req.body
+        //owner from req.user._id
+        const task = new Task({
+            ...req.body,
+            owner: req.user._id
+        })
+        await task.save();
+        res.status(201).json({ task, message: 'Task created successfully' });
+    }
+    catch(err) {
+        res.status(400).send({ error: err });
+    }
 })
 
 module.exports = router;
